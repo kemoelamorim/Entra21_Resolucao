bad741d6c66e969cc37990d2e5552f3009f9ba82
Kemoel <kemoel.amorim@gmail.com>
2020-10-16 12:52:29 -0300

    Loop para cadastros

--- Resolucao_Aula008/file_controls.py
+++ Resolucao_Aula008/file_controls.py
@@ -7,19 +7,19 @@ def cria_newFile(pessoa):
     arquivo.close()
     return 'Primeiro Cadastro salvo'
 
-# w = cria um novo arquivo, porem se o arquivo existir ele apaga o antigo
-def criar_e_substituiFile(pessoa):
-    arquivo = open('Resolucao_Aula008/files/pessoa.txt','w')
-    arquivo.write(f"{pessoa['nome']};{pessoa['sobrenome']};{pessoa['idade']};{pessoa['i_d']} ")
-    arquivo.close()
-    return 'Criou e subistituiu cadastro'
-
 # a = adiciona uma nova linha ao final do arquivo
 def add_File(pessoa):
     arquivo = open('Resolucao_Aula008/files/pessoa.txt','a')
     arquivo.write(f"{pessoa['nome']};{pessoa['sobrenome']};{pessoa['idade']};{pessoa['i_d']} ")
     arquivo.close()
     return 'Cadastro adicionado e salvo'
+
+# w = cria um novo arquivo, porem se o arquivo existir ele apaga o antigo
+def criar_e_substituiFile(pessoa):
+    arquivo = open('Resolucao_Aula008/files/pessoa.txt','w')
+    arquivo.write(f"{pessoa['nome']};{pessoa['sobrenome']};{pessoa['idade']};{pessoa['i_d']} ")
+    arquivo.close()
+    return 'Criou e subistituiu cadastro'
 
 # leitura de arquivo
 def writeFile():
--- Resolucao_Aula008/main.py
+++ Resolucao_Aula008/main.py
@@ -2,65 +2,68 @@ from funcoes import cadastrar_pessoa,  listar_pessoas_cadastradas, pessoas, pess
 from funcoes import cadastrar_endereco, listar_enderecos_cadastrados, enderecos, endereco_especifico
 from file_controls import*
 
-# Dados para o cadastro de Pessoas
-print('Cadastrando pessoas')
-nome = input("Digite seu nome: ")
-sobrenome = input("Digite seu sobrenome: ")
-idade = int(input("Digite sua idade: "))
+cadastrando = True
+while cadastrando:
+    # Dados para o cadastro de Pessoas
+    print('Cadastrando pessoas')
+    nome = input("Digite seu nome: ")
+    sobrenome = input("Digite seu sobrenome: ")
+    idade = int(input("Digite sua idade: "))
 
-pessoa = cadastrar_pessoa(nome, sobrenome, idade)
+    pessoa = cadastrar_pessoa(nome, sobrenome, idade)
 
-# Dados para o cadastro de Endereços
-i_d = (len(pessoas))# ((pessoa['i_d'])for pessoa in pessoas)
-rua = input('Digite sua rua: ')
-numero = input('Digite seu numero: ')
-complemento = input('Digite o complemento: ')
-bairro = input('Digite o seu bairro: ')
-cidade = input('Digite a sua cidade: ')
-estado = input('Digite seu Estado: ')
+    # Dados para o cadastro de Endereços
+    i_d = (len(pessoas))# ((pessoa['i_d'])for pessoa in pessoas)
+    rua = input('Digite sua rua: ')
+    numero = input('Digite seu numero: ')
+    complemento = input('Digite o complemento: ')
+    bairro = input('Digite o seu bairro: ')
+    cidade = input('Digite a sua cidade: ')
+    estado = input('Digite seu Estado: ')
 
-endereco = cadastrar_endereco(i_d, rua, numero, complemento, bairro, cidade, estado)
+    endereco = cadastrar_endereco(i_d, rua, numero, complemento, bairro, cidade, estado)
 
-# Listar Pessoas
-dictPessoas = listar_pessoas_cadastradas(pessoas)
-print(dictPessoas)
+    # Listar Pessoas
+    dictPessoas = listar_pessoas_cadastradas(pessoas)
+    print(dictPessoas)
 
-# Buscas Pessoa específica
-buscaPessoa = int(input('Buscar pessoa por ID: '))
-pessoaEspecifia = pessoa_especifica(buscaPessoa)
-print(pessoaEspecifia)
+    # Buscas Pessoa específica
+    buscaPessoa = int(input('Buscar pessoa por ID: '))
+    pessoaEspecifia = pessoa_especifica(buscaPessoa)
+    print(pessoaEspecifia)
 
-# Listar Endereços
-dictEnderecos = listar_enderecos_cadastrados(enderecos)
-print(dictEnderecos)
+    # Listar Endereços
+    dictEnderecos = listar_enderecos_cadastrados(enderecos)
+    print(dictEnderecos)
 
-# Busca endereço especifico
-buscaEndereco = int(input('Buscar endereço por ID: '))
-enderecoEspecifico = endereco_especifico(buscaEndereco)
-print(enderecoEspecifico)
+    # Busca endereço especifico
+    buscaEndereco = int(input('Buscar endereço por ID: '))
+    enderecoEspecifico = endereco_especifico(buscaEndereco)
+    print(enderecoEspecifico)
 
-# manipulação de arquivos
-print(""" 
-O que deseja fazer com cadastros?
-1 - Primeiro cadastro 
-2 - Adicionando e Salvando
-3 - Salvar e Substituir
-4 - Ver cadastros salvos
-5 - Sair sem Salvar
-""")
-escolha_menu = input('Digite a opção Desejada?: ')
+    # manipulação de arquivos
+    print(""" 
+    O que deseja fazer com cadastros?
+    1 - Primeiro cadastro 
+    2 - Adicionando e Salvando
+    3 - Salvar e Substituir
+    4 - Ver cadastros salvos
+    5 - Sair sem Salvar
+    """)
+    escolha_menu = input('Digite a opção Desejada?: ')
 
-if escolha_menu == '1':
-    status = cria_newFile(pessoa)
-    print(status)
-elif escolha_menu == '2':
-    status = add_File(pessoa)
-    print(status)
-elif escolha_menu == '3':        
-    status = criar_e_substituiFile(pessoa)
-    print(status)
-elif escolha_menu == '4':
-    status = writeFile()
-    print(status)
-else:
-    print('Até a próxima!')
+    if escolha_menu == '1':
+        status = cria_newFile(pessoa)
+        print(status)
+    elif escolha_menu == '2':
+        status = add_File(pessoa)
+        print(status)
+    elif escolha_menu == '3':        
+        status = criar_e_substituiFile(pessoa)
+        print(status)
+    elif escolha_menu == '4':
+        status = writeFile()
+        print(status)
+    else:
+        print('Até a próxima!')
+        cadastrando = False
